a82fc17649e9a00b47acffe6779bbd855fe30466
Miss ba <missba@pop-os.localdomain>
2021-04-11 14:45:13 +0530

    Added and styled contact info icons

--- src/components/contact.js
+++ src/components/contact.js
@@ -1,8 +1,27 @@
+import gitImage from "../assests/git.svg"
+import linkedinImage from "../assests/LINKDN.svg"
+import gmailImage from "../assests/Gmail-logo.png"
 export default function Contact() {
     return (
-       <> <p> Git:""  </p> 
-        <p> Linkden:"insert link" </p>
+       <div>
+
+       <div>
+       <p> Github </p> 
+        <img className="icon"id ="git-icon"src = {gitImage} alt="git icon"/> 
+        </div>
+        <div>
+        <p> Linkedin </p>
+        <img className="icon" id ="linkedin-icon"src = {linkedinImage} alt="linkedin icon"/>
+        
+        </div>
+        <div>
         <p> Email Id: misbafarheen309@gmail.com </p>
-        <p> feedback thingy </p> </>
+        <img className="icon" id="gmail-icon" src={gmailImage} alt="gmail icon"/>
+        </div>
+        <div>
+        <p> feedback thingy </p>
+        </div>
+        
+        </div>
     )
 }
